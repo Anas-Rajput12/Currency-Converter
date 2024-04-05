d5fa4a6fcbabf7a53866084e8be9578f5fa8073a
Muhammad Anas Qadri <161422002+Anas-Rajput12@users.noreply.github.com>
2024-04-06 00:39:16 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+import inquirer from 'inquirer';
+const currency = {
+    USD: 1,
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280
+};
+async function data() {
+    let atm1 = true;
+    do {
+        const answer = await inquirer.prompt([
+            {
+                name: "from",
+                message: "Enter from currency : ",
+                type: "list",
+                choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
+            },
+            {
+                name: "to",
+                message: "Enter from currency : ",
+                type: "list",
+                choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
+            },
+            {
+                name: "amount",
+                message: "Enter Your Amount",
+                type: "number",
+            }
+        ]);
+        let fromAmount = currency[answer.from];
+        let toAmount = currency[answer.to];
+        let amount = answer.amount;
+        let baseAmount = amount / fromAmount;
+        let convertedAmount = baseAmount * toAmount;
+        console.log(convertedAmount);
+        const { atmdata } = await inquirer.prompt([
+            {
+                name: "atmdata",
+                type: "confirm",
+                message: "Funther check your Transaction ?",
+                default: true
+            }
+        ]);
+        atm1 = atmdata;
+    } while (atm1);
+}
+data();
